682b7239c2d8470aa54f8fc992f3cb37ee59d636
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-05-12 17:22:30 +0530

    updated code

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from GitHub Actions CI/CD!');
+    res.send('Hello from GitHub Actions CI/CD! 123');
 });
 
 export default app;
